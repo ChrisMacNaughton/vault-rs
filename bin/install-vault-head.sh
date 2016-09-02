c5e8df3306885cdf40f529d0c67a8fd80e74fe3c
Chris MacNaughton <chmacnaughton@gmail.com>
2016-09-02 08:57:59 -0400

    update to build vault from HEAD

--- bin/install-vault-head.sh
+++ bin/install-vault-head.sh
@@ -4,15 +4,15 @@ set -eux
 mkdir -p $HOME/bin
 
 export GOPATH=$HOME/go
-mkdir $GOPATH
+mkdir -p $GOPATH
 
 export PATH=$GOPATH/bin:$PATH
 
-go get github.com/tools/godep
-go get github.com/mitchellh/gox
-
 git clone https://github.com/hashicorp/vault.git $GOPATH/src/github.com/hashicorp/vault
 cd $GOPATH/src/github.com/hashicorp/vault
+make bootstrap
+# go get github.com/tools/godep
+# go get github.com/mitchellh/gox
 make dev
 
 mv bin/vault $HOME/bin
