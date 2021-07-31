7b8e2c1d7c70cae047b94d1a2cf4568e55bff39b
Oscar Rainford <oscar@fourbs.com.au>
2021-07-31 20:48:37 +1000

    chore(deps): update dependencies.

--- src/client/error.rs
+++ src/client/error.rs
@@ -8,48 +8,41 @@ quick_error! {
         /// `reqwest::Error` errors
         Reqwest(err: ::reqwest::Error) {
             from()
-            description("reqwest error")
             display("reqwest error: {}", err)
-            cause(err)
+            source(err)
         }
         /// `serde_json::Error`
         SerdeJson(err: ::serde_json::Error) {
             from()
-            description("serde_json Error")
             display("serde_json Error: {}", err)
-            cause(err)
+            source(err)
         }
         /// Vault errors
         Vault(err: String) {
-            description("vault error")
             display("vault error: {}", err)
         }
         /// Response from Vault errors
         /// This is for when the response is not successful.
         VaultResponse(err: String, response: reqwest::blocking::Response) {
-            description("vault response error")
             display("Error in vault response: {}", err)
         }
         /// IO errors
         Io(err: ::std::io::Error) {
             from()
-            description("io error")
             display("io error: {}", err)
-            cause(err)
+            source(err)
         }
         /// `Url` parsing error
         Url(err: ::url::ParseError) {
             from()
-            description("url parse error")
             display("url parse error: {}", err)
-            cause(err)
+            source(err)
         }
         /// `Base64` decode error
         Base64(err: ::base64::DecodeError) {
             from()
-                description("base64 decode error")
-                display("base64 decode error: {}", err)
-                cause(err)
+            display("base64 decode error: {}", err)
+            source(err)
         }
     }
 }
